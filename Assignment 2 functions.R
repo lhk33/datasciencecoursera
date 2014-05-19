d8630ab6753c5544afed6114d2abaf1b0ba61157
lhk33 <lawryn.kasper@stjude.org>
2014-05-18 21:46:23 -0500

    add file with makeCacheMatrix and cacheSolve functions for R assignment 2

---
+++ Assignment 2 functions.R
@@ -0,0 +1,37 @@
+## makeCacheMatrix is a function of four functions needed to set up 
+## a caching matrix, get the data for the original matrix, invert the 
+## matrix and cache the inverted matrix
+makeCacheMatrix <- function(x = matrix()) {  
+        inv <- NULL
+        ## set takes values for x (the intial matrix) and nulls cache
+        ## matrix (inv)
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        ## get returns x (the initial matrix)
+        get <- function() x
+        ## setSolve creates the inverted matrix and assigns it to inv
+        setSolve <- function(solve) inv <<- solve
+        ## getSolve returns inv (the inverted matrix)
+        getSolve <- function() inv
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
+}  
+
+## cacheSolve returns the cached inverted matrix for the matrix of interest
+## if the cache matrix (inv) is not null.  If the cach matrix is null,  it 
+## calculates the inverted matrix for the matrix of interest and caches
+## the result to inv and returns the cached inverted matrix.
+cacheSolve <- function(x, ...) {
+        inv <- x$getSolve()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setSolve(inv)
+        inv
+}
---
+++ push_to_GitHub_sequence.md
@@ -0,0 +1,7 @@
+git add .
+git commit -m "message"
+git push
+
+LHK33 username for swirl
+Type swirl() to resume
+
